c4e87ee7b5fa71c0507e98655c8319adb4f902f8
snehal12s <snehalhalasge@gmail>
2026-04-01 11:03:37 +0530

    initial commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,13 +1,22 @@
 plugins {
     id("com.android.application")
+<<<<<<< HEAD
     id("com.google.gms.google-services") // This matches the one we added above
     id("kotlin-android")
+=======
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+>>>>>>> 386a876 (initial commit)
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.example.revolveagro"
+<<<<<<< HEAD
     compileSdk = 36
+=======
+    compileSdk = flutter.compileSdkVersion
+>>>>>>> 386a876 (initial commit)
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -25,7 +34,11 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
+<<<<<<< HEAD
         targetSdk = 36
+=======
+        targetSdk = flutter.targetSdkVersion
+>>>>>>> 386a876 (initial commit)
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
