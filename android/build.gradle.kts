0f95c0b88305d32f4a5ab6c9bc53ea51ebd3a9ec
samarthhatte <109466822+samarthhatte@users.noreply.github.com>
2026-04-06 11:51:29 +0530

    updated gradle.build.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,14 +1,12 @@
-<<<<<<< HEAD
-=======
+// 🔥 Plugins section merged from incoming branch
 plugins {
-   id("com.android.application") apply false
-   id("com.android.library") apply false
-   id("org.jetbrains.kotlin.android") apply false
-   id("dev.flutter.flutter-gradle-plugin") apply false
-   id("com.google.gms.google-services") apply false
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    id("org.jetbrains.kotlin.android") apply false
+    id("dev.flutter.flutter-gradle-plugin") apply false
+    id("com.google.gms.google-services") apply false
 }
 
->>>>>>> 4de844c681abb2fbeb86804d77c2f9ebf4a02000
 allprojects {
     repositories {
         google()
@@ -34,6 +32,7 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
